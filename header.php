7af915263cbceffd2b6949f66e56002a3969a4dd
iemadi <adivijay500@gmail.com>
2021-12-12 07:13:19 -0800

    my best teen patti website

---
+++ header.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>New Teen Patti Apps - Play & Earn</title>
+    <meta name="description" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Add site Favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+
+    <!-- CSS 
+    ========================= -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Metal+Mania&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="body__bg" data-bgimg="assets/img/bg/body-bg.webp">
+   
+  
+     <!--offcanvas menu area start-->
+     <div class="body_overlay">
+                
+    </div>
+    <div class="offcanvas_menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="offcanvas_menu_wrapper">
+                        <div class="canvas_close">
+                            <a href="javascript:void(0)"><i class="icofont-close"></i></a>  
+                        </div>
+                        <div id="menu" class="text-left ">
+                            <ul class="offcanvas_main_menu">
+                                <li class="menu-item-has-children active">
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#hot-games">Hot Games</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#download-apps">Download</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#faq">FAQ</a>
+                                </li>
+                                <!-- <li class="menu-item-has-children"><a href="#">Match</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="match.html">Match Page</a></li>
+                                        <li><a href="match-details.html">Match Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children"><a href="#">Pages</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="all-game.html">All Game</a></li>
+                                        <li><a href="game-details.html">Game Details</a></li>
+                                        <li><a href="faq.html">Faq Page</a></li>
+                                        <li><a href="players.html">Players</a></li>
+                                        <li><a href="player-details.html">Player Details</a></li>
+                                        <li><a href="404.html">Error 404</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                        <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                        <li><a href="blog-without-sidebar.html">Blog Without Sidebar</a></li>
+                                         <li><a href="blog-grid-left-sidebar.html">Blog Grid Left Sidebar</a></li>
+                                            <li><a href="blog-grid-right-sidebar.html">Blog Grid Right Sidebar</a></li>
+                                        <li><a href="blog-grid-without-sidebar.html">Blog Grid Without Sidebar</a></li>
+                                        <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
+                                        <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li> -->
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--offcanvas menu area end-->
+
+    <!--header area start-->
+    <header class="header_section header_transparent sticky-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="main_header d-flex justify-content-between align-items-center">
+                        <div class="header_logo">
+                            <a class="sticky_none" href="index.html"><img src="assets/img/logo/logo.png" alt="" width="100"></a>
+                        </div>
+                        <!--main menu start-->
+                        <div class="main_menu d-none d-lg-block"> 
+                            <nav>  
+                                <ul class="d-flex">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="#hot-games">Hot Games</a></li>
+                                    <li><a href="#download-apps">Download</a></li>
+                                    <li><a href="#faq">FAQ</a></li>
+                                    <!-- <li><a href="match.html">Match</a>
+                                        <ul class="sub_menu">
+                                            <li><a href="match.html">Match Page</a></li>
+                                            <li><a href="match-details.html">Match Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="sub_menu">
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="all-game.html">All Game</a></li>
+                                            <li><a href="game-details.html">Game Details</a></li>
+                                            <li><a href="faq.html">Faq Page</a></li>
+                                            <li><a href="players.html">Players</a></li>
+                                            <li><a href="player-details.html">Player Details</a></li>
+                                            <li><a href="404.html">Error 404</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="blog-left-sidebar.html">blog</a>
+                                        <ul class="sub_menu">
+                                            <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                            <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                            <li><a href="blog-without-sidebar.html">Blog Without Sidebar</a></li>
+                                             <li><a href="blog-grid-left-sidebar.html">Blog Grid Left Sidebar</a></li>
+                                            <li><a href="blog-grid-right-sidebar.html">Blog Grid Right Sidebar</a></li>
+                                            <li><a href="blog-grid-without-sidebar.html">Blog Grid Without Sidebar</a></li>
+                                            <li><a href="blog-details-left-sidebar.html">Blog Details Left Sidebar</a></li>
+                                            <li><a href="blog-details-right-sidebar.html">Blog Details Right Sidebar</a></li>
+                                            <li><a href="blog-details.html">Blog Details</a></li>
+                                        </ul>
+                                    </li> -->
+                                    
+                                </ul>  
+                            </nav>
+                        </div>
+                        <!--main menu end-->
+                        <div class="header_right_sidebar d-flex align-items-center">
+                            <!-- <div class="sing_up_btn">
+                                <a class="btn btn-link" href="#">SIGN UP <img src="assets/img/icon/arrrow-icon2.webp" alt=""> </a>
+                            </div> -->
+                            <div class="canvas_open">
+                                <a href="javascript:void(0)"><i class="icofont-navigation-menu"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--header area end-->
---
+++ index.php
@@ -0,0 +1,199 @@
+<?php include('header.php') ?>
+
+    <!-- page wrapper start -->
+    <div class="page_wrapper">
+
+        <!--slide banner section start-->
+        <section class="hero_banner_section d-flex align-items-center">
+            <div class="container">
+                <div class="hero_banner_inner">
+                    <div class="row align-items-center">
+                        <div class="col-12">
+                            <div class="hero_content">
+                                <h1 class="wow fadeInUp" data-wow-delay="0.1s" data-wow-duration="1.1s">Play Now <br>
+                                    Earn Today.</h1>
+                                <p class="wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1.2s">Download World's Popular MULTIPLAYER Card Game Teen Patti Plus & Get upto Ten Lakh Free CHIPS! Get Highest Daily Bonuses.</p>
+                                <a class="btn btn-link wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="1.3s" href="https://nn5.pw/a/hhmxi">Download Now <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        </section>
+        <!--slider area end-->
+
+        <!-- gaming  world section start -->
+        <section class="gaming_world_section mb-140" id="hot-games">
+            <div class="container">
+                <div class="section_title text-center wow fadeInUp mb-60" data-wow-delay="0.1s" data-wow-duration="1.1s">
+                    <h2>Hot Games</h2>
+                </div>
+                <div class="gaming_world_inner">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="single_gaming_world wow fadeInUp" data-bgimg="assets/img/others/gaming-world-bg1.webp" data-wow-delay="0.1s" data-wow-duration="1.1s">
+                                <div class="gaming_world_thumb">
+                                    <img src="assets/img/teenpattiplus.png" alt="">
+                                </div>
+                                <div class="gaming_world_text">
+                                    <h3>TeenPatti Plus</h3>
+                                    <p>Download World's Popular MULTIPLAYER Card Game Teen Patti Plus & Get upto Ten Lakh Free CHIPS! Get Highest Daily Bonuses.</p>
+                                    <a class="btn btn-link wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="1.3s" href="https://nn5.pw/a/hhmxi">Download Now <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="single_gaming_world wow fadeInUp" data-bgimg="assets/img/others/gaming-world-bg2.webp" data-wow-delay="0.2s" data-wow-duration="1.2s">
+                                <div class="gaming_world_thumb">
+                                    <img src="assets/img/teenpattimaster.png" alt="">
+                                </div>
+                                <div class="gaming_world_text">
+                                    <h3>TeenPatti Master</h3>
+                                    <p>Download World's Popular MULTIPLAYER Card Game TeenPatti Master & Get upto Ten Lakh Free CHIPS! Get Highest Daily Bonuses.</p>
+                                    <a class="btn btn-link wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="1.3s" href="https://nn5.pw/aw/hhmxi">Download Now <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="single_gaming_world wow fadeInUp" data-bgimg="assets/img/others/gaming-world-bg3.webp" data-wow-delay="0.3s" data-wow-duration="1.3s">
+                                <div class="gaming_world_thumb">
+                                    <img src="assets/img/teenpattione.png" alt="">
+                                </div>
+                                <div class="gaming_world_text">
+                                    <h3>TeenPatti One</h3>
+                                    <p>Download World's Popular MULTIPLAYER Card Game TeenPatti One & Get upto Ten Lakh Free CHIPS! Get Highest Daily Bonuses.</p>
+                                    <a class="btn btn-link wow fadeInUp" data-wow-delay="0.3s" data-wow-duration="1.3s" href="https://hh7.pw/a/2/hhmxi">Download Now <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- gaming  world section end -->
+
+        <!-- popular gaming  section start -->
+        <section class="popular_gaming_section mb-140" id="download-apps">
+            <div class="container">
+                <div class="section_title text-center wow fadeInUp mb-60" data-wow-delay="0.1s" data-wow-duration="1.1s">
+                    <h2>Popular GAME</h2>
+                    <p>Win Real Rupees in TeenPatti Plus</p>
+                </div>
+                <div class="popular_gaming_inner wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1.2s">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6">
+                            <div class="popular_gaming_thumb">
+                            <a href="#"><img src="assets/img/teenpattiplus.jpg" alt=""></a>
+                            <div class="gaming_details_btn">
+                                    <a class="btn btn-link" href="https://nn5.pw/a/hhmxi">Download APK <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <div class="popular_gaming_thumb">
+                            <a href="#"><img src="assets/img/teenpattimaster.jpg" alt=""></a>
+                            <div class="gaming_details_btn">
+                                    <a class="btn btn-link" href="https://nn5.pw/aw/hhmxi">Download APK <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <div class="popular_gaming_thumb">
+                            <a href="#"><img src="assets/img/teenpattione.jpg" alt=""></a>
+                                <div class="gaming_details_btn">
+                                    <a class="btn btn-link" href="https://hh7.pw/a/2/hhmxi">Download APK <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6">
+                            <div class="popular_gaming_thumb">
+                            <a href="#"><img src="assets/img/comingsoon.jpg" alt=""></a>
+                                <!-- <div class="gaming_details_btn">
+                                    <a class="btn btn-link" href="game-details.html">Download APK <img src="assets/img/icon/arrrow-icon.webp" alt=""> </a>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- popular gaming section end -->
+
+        <!-- faq page section start -->
+        <section class="faq_page_section mb-140" id="faq">
+            <div class="container">
+                <div class="section_title text-center wow fadeInUp mb-60" data-wow-delay="0.1s" data-wow-duration="1.1s">
+                    <h2>FAQ</h2>
+                </div>
+                <div class="faq_page_inner">
+                    <div class="accordion" id="accordionExample">
+                        <div class="faq_question_list">
+                          <h2 class="questions__title accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            What is Teenpatti Plus?
+                            </button>
+                          </h2>
+                          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="frequently_desc accordion-body">
+                                <p>Teenpatti Plus is India`s best free online games.More hot games.more rewards!Get free chips and win real cash!Free for limited time!Go download it now!</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="faq_question_list">
+                          <h2 class="questions__title accordion-header" id="headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            How to earn cash with Teenpatti Plus Games?
+                            </button>
+                          </h2>
+                          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="frequently_desc accordion-body">
+                                <p>First,you should download Teenpatti Plus to get free money,then you can show your skills to win more and invite friends to get more rewards.</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="faq_question_list">
+                            <h2 class="questions__title accordion-header" id="headingthree">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsethree" aria-expanded="false" aria-controls="collapsethree">
+                              How can I contact Teenpatti Plus Games Team?
+                              </button>
+                            </h2>
+                            <div id="collapsethree" class="accordion-collapse collapse" aria-labelledby="headingthree" data-bs-parent="#accordionExample">
+                                <div class="frequently_desc accordion-body">
+                                  <p>Do not hesitate to contact us through email at Teenpattiplus@gmail.com.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq_question_list">
+                            <h2 class="questions__title accordion-header" id="headingfour">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                              Is there a limit to how much money I can win in this game?
+                              </button>
+                            </h2>
+                            <div id="collapsefour" class="accordion-collapse collapse" aria-labelledby="headingfour" data-bs-parent="#accordionExample">
+                                <div class="frequently_desc accordion-body">
+                                  <p>No, there is no limit to the money you can win in the game, but Teen Patti Plus keep track of the behaviors of game account all the time to ensure the compliance of game rules and fairness of the game.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="faq_question_list">
+                            <h2 class="questions__title accordion-header" id="headingfive">
+                              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                              what is the maximum withdrawal money for per day?
+                              </button>
+                            </h2>
+                            <div id="collapsefive" class="accordion-collapse collapse" aria-labelledby="headingfive" data-bs-parent="#accordionExample">
+                                <div class="frequently_desc accordion-body">
+                                  <p>In the Teen Patti Plus game, the user's daily withdrawal limit is 3 times and the withdrawal limit is 15000 Rs for per day.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- faq page section end -->
+
+    </div>
+    <!-- page wrapper end -->
+    <?php include('footer.php') ?>
+    
